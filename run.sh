99e81f7d7dcfdfba0103100ffdd3da16b3712cff
Jonas G. Drange <jonas.drange@free2.no>
2017-11-23 20:32:18 +0100

    configures nxing/docker

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
 SETTINGS=${DJANGO_SETTINGS_MODULE:-sausite.settings}
 
-echo $PYTHONPATH
+rm /etc/nginx/sites-enabled/default
+ln -s /configs/nginx.conf /etc/nginx/sites-enabled/sau
+
+/etc/init.d/nginx restart
+
 echo Starting Gunicorn.
 echo Using settings $SETTINGS
 exec gunicorn --env DJANGO_SETTINGS_MODULE=$SETTINGS sausite.wsgi:application \
-    --bind 0.0.0.0:8000 \
+    --bind 0.0.0.0:8001 \
     --workers 3
