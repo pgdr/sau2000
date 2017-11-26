be3d868dfe02a5e93d2c627c00243276130aca0d
Pål Grønås Drange <PGDR@statoil.com>
2017-11-26 21:35:08 +0100

    Add Medicine, Dose to admin panel

--- sau/admin.py
+++ sau/admin.py
@@ -4,5 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Sheep
+from .models import Sheep, Medicine, Dose
 admin.site.register(Sheep)
+admin.site.register(Medicine)
+admin.site.register(Dose)
