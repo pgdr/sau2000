484b4c4c3be67aa877a529ad0e5459573be28eb5
Pål Grønås Drange <PGDR@statoil.com>
2017-12-04 21:34:11 +0100

    Add gender aware parent dropdown list

--- sau/admin.py
+++ sau/admin.py
@@ -3,8 +3,24 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from django import forms
+
 # Register your models here.
 from .models import Sheep, Medicine, Dose
-admin.site.register(Sheep)
+
+
+# To get gender-aware dropdown list for parent of a sheep
+class SheepParentForm(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "father":
+            kwargs["queryset"] = Sheep.objects.filter(sex='m')  # male
+        elif db_field.name == "mother":
+            kwargs["queryset"] = Sheep.objects.filter(sex='f')  # female
+        return super(SheepParentForm, self).formfield_for_foreignkey(db_field,
+                                                                     request,
+                                                                     **kwargs)
+
+
+admin.site.register(Sheep, SheepParentForm)
 admin.site.register(Medicine)
 admin.site.register(Dose)
