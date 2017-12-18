098a35067351551cdb321d0cb85a338110bc345a
Jonas G. Drange <jonas@drange.net>
2017-12-18 20:28:07 +0100

    Let admin be farm-aware.

--- sau/admin.py
+++ sau/admin.py
@@ -2,20 +2,28 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Sheep, Medicine, Dose, Farm
 
-from django import forms
 
-# Register your models here.
-from .models import Sheep, Medicine, Dose
+class FarmAwareModelAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(farm__farmers__in=[request.user])
 
 
 # To get gender-aware dropdown list for parent of a sheep
-class SheepParentForm(admin.ModelAdmin):
+class SheepParentForm(FarmAwareModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "father":
-            kwargs["queryset"] = Sheep.objects.filter(sex='m')  # male
-        elif db_field.name == "mother":
-            kwargs["queryset"] = Sheep.objects.filter(sex='f')  # female
+        filters = {}
+        if not request.user.is_superuser:
+            filters['farm__farmers__in'] = [request.user]
+        if db_field.name == 'father':
+            filters['sex'] = 'm'  # male
+        elif db_field.name == 'mother':
+            filters['sex'] = 'f'  # female
+        kwargs['queryset'] = Sheep.objects.filter(**filters)
         return super(SheepParentForm, self).formfield_for_foreignkey(db_field,
                                                                      request,
                                                                      **kwargs)
@@ -24,3 +32,4 @@ class SheepParentForm(admin.ModelAdmin):
 admin.site.register(Sheep, SheepParentForm)
 admin.site.register(Medicine)
 admin.site.register(Dose)
+admin.site.register(Farm, FarmAwareModelAdmin)
