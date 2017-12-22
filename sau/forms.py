a5d0cfac508a853b0b8735ee53ec378c42f99fbd
Pål Grønås Drange <PGDR@statoil.com>
2017-12-22 13:26:18 +0100

    Add Dose form for sheep

---
+++ sau/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Sheep, Dose
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class DoseForm(forms.ModelForm):
+
+    class Meta:
+        model = Dose
+        fields = ('medicine', 'amount', 'date_utc')
+        widgets = {
+            'date_utc': DateInput()
+        }
--- sau/views.py
+++ sau/views.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from datetime import datetime
+from datetime import datetime as dt
 
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
-from django.shortcuts import get_object_or_404
-
+from django.shortcuts import get_object_or_404, redirect
 from sau.models import Sheep, Dose, Farm
+from .forms import DoseForm
+
 
 SAUS = [
     {
         'name': 'britanna',
-        'birth_date_utc': datetime.now(),
+        'birth_date_utc': dt.now(),
         'sex': 'f',
     },
     {
         'name': 'lolcakes',
-        'birth_date_utc': datetime.now(),
+        'birth_date_utc': dt.now(),
         'sex': 'f',
         'ear_tag': '2608',
         'ear_tag_color': 'r',
@@ -24,7 +25,7 @@ SAUS = [
     },
     {
         'name': 'rambo',
-        'birth_date_utc': datetime.now(),
+        'birth_date_utc': dt.now(),
         'sex': 'm',
         'quality': 'p-',
     },
@@ -54,14 +55,40 @@ def index(request):
     sheep = get_all_sheep(request)
     return TemplateResponse(request, 'index.html', context={'sheep': sheep})
 
+def _get_sheep_or_404(request, slug):
+    if request.user.is_superuser:
+        return get_object_or_404(Sheep, slug=slug)
+
+    return get_object_or_404(Sheep, slug=slug,
+                             farm__farmers__in=[request.user])
 
 @login_required
 def sau(request, slug=""):
-    if request.user.is_superuser:
-        sheep = get_object_or_404(Sheep, slug=slug)
-    else:
-        sheep = get_object_or_404(Sheep, slug=slug,
-                                  farm__farmers__in=[request.user])
+    sheep = _get_sheep_or_404(request, slug)
     doses = Dose.get(sheep=sheep)
     return TemplateResponse(request, 'sau.html', context={'sheep': sheep,
                                                           'doses': doses})
+
+
+@login_required
+def _do_the_post(request, slug):
+    form = DoseForm(request.POST)
+    if form.is_valid():
+        dose = form.save(commit=False)
+        dose.sheep = _get_sheep_or_404(request, slug)
+        h,m = dt.now().hour, dt.now().minute
+        dose.date_utc = dose.date_utc.replace(hour=h, minute=m, second=0)
+        dose.save()
+    return redirect('sau', slug=slug)
+
+
+@login_required
+def dose(request, slug=''):
+    if request.method == "POST":
+        return _do_the_post(request, slug)
+    sheep = _get_sheep_or_404(request, slug)
+    form = DoseForm(initial={'sheep': sheep,
+                             'date_utc': dt.now()})
+    return TemplateResponse(request,
+                            'dose.html',
+                            context={'form': form, 'sheep': sheep})
