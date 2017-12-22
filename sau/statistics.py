4e4a0701529ed9ef2393227f8fe90a1c3fbc36ab
Pål Grønås Drange <PGDR@statoil.com>
2017-12-22 18:08:23 +0100

    Fix no weight bug, add lamb per year stats

--- sau/statistics.py
+++ sau/statistics.py
@@ -1,10 +1,25 @@
 from sau.models import QUALITIES
+from datetime import datetime as dt
+
+def _lambs_per_year(sheeps):
+    if not sheeps:
+        return [], []
+    current_year = dt.now().year
+    start_year = min([s.birth_date_utc.year for s in sheeps])
+
+    year_labels = list(range(start_year, current_year+1))
+    year_lst = [0] * len(year_labels)
+    for s in sheeps:
+        y = s.birth_date_utc.year
+        idx = year_labels.index(y)
+        year_lst[idx] += 1
+    return year_lst, year_labels
 
 
 def _weights(sheeps):
     if not sheeps:
         return [], []
-    weights = [s.weight for s in sheeps]
+    weights = [s.weight for s in sheeps if s.weight]
     upper = max(weights)
     weight_labels = list(range(0, int(upper + 2), 200))
     weight_lst = [0] * len(weight_labels)
@@ -18,7 +33,7 @@ def _weights(sheeps):
 def _qualities(sheeps):
     if not sheeps:
         return [], []
-    qualities = [s.quality for s in sheeps]
+    qualities = [s.quality for s in sheeps if s.quality]
     quality_lst = []
     quality_labels = []
     for Q in QUALITIES:
@@ -28,13 +43,15 @@ def _qualities(sheeps):
     return quality_lst, quality_labels
 
 
-def get_statistics(sheeps):
+def get_statistics(dead_sheeps, all_sheeps):
     """Return qualities_count/quality_labels, weight_count/weight_labels for use in
     barchart.
 
     """
 
-    quality_lst, quality_labels = _qualities(sheeps)
-    weight_lst, weight_labels = _weights(sheeps)
+    quality_lst, quality_labels = _qualities(dead_sheeps)
+    weight_lst, weight_labels = _weights(dead_sheeps)
+    lambyear_lst, lambyear_labels = _lambs_per_year(all_sheeps)
 
-    return quality_lst, quality_labels, weight_lst, weight_labels
+    return (quality_lst, quality_labels, weight_lst, weight_labels,
+            lambyear_lst, lambyear_labels)
