04739982194c69755302a2e03363f534118defa6
Pål Grønås Drange <PGDR@statoil.com>
2017-12-22 14:13:36 +0100

    Fix 404 and return form on invalid

--- sau/views.py
+++ sau/views.py
@@ -5,10 +5,13 @@ from datetime import datetime as dt
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
 from django.shortcuts import get_object_or_404, redirect
+from django.http import Http404
+
 from sau.models import Sheep, Dose, Farm
 from .forms import DoseForm
 
 
+# SAUS exists solely to populate an empty DB with some stuff.  Will be removed.
 SAUS = [
     {
         'name': 'britanna',
@@ -55,40 +58,64 @@ def index(request):
     sheep = get_all_sheep(request)
     return TemplateResponse(request, 'index.html', context={'sheep': sheep})
 
+
 def _get_sheep_or_404(request, slug):
     if request.user.is_superuser:
         return get_object_or_404(Sheep, slug=slug)
 
-    return get_object_or_404(Sheep, slug=slug,
-                             farm__farmers__in=[request.user])
+    return get_object_or_404(
+        Sheep, slug=slug, farm__farmers__in=[request.user])
+
 
 @login_required
 def sau(request, slug=""):
     sheep = _get_sheep_or_404(request, slug)
     doses = Dose.get(sheep=sheep)
-    return TemplateResponse(request, 'sau.html', context={'sheep': sheep,
-                                                          'doses': doses})
+    return TemplateResponse(
+        request, 'sau.html', context={
+            'sheep': sheep,
+            'doses': doses
+        })
 
 
 @login_required
-def _do_the_post(request, slug):
+def _save_dose(request, slug):
     form = DoseForm(request.POST)
     if form.is_valid():
         dose = form.save(commit=False)
         dose.sheep = _get_sheep_or_404(request, slug)
-        h,m = dt.now().hour, dt.now().minute
+        h, m = dt.now().hour, dt.now().minute
         dose.date_utc = dose.date_utc.replace(hour=h, minute=m, second=0)
         dose.save()
-    return redirect('sau', slug=slug)
+    else:
+        raise ValueError('Invalid form for dose: %s' % str(request.POST))
 
 
 @login_required
 def dose(request, slug=''):
+    if request.method not in ('POST', 'GET'):
+        raise Http404
+
+    # on (method.POST and form.valid) we redirect to sau, else we return
+    # TemplateResponse with form
+
+    current_sheep = _get_sheep_or_404(request, slug)
+
     if request.method == "POST":
-        return _do_the_post(request, slug)
-    sheep = _get_sheep_or_404(request, slug)
-    form = DoseForm(initial={'sheep': sheep,
-                             'date_utc': dt.now()})
-    return TemplateResponse(request,
-                            'dose.html',
-                            context={'form': form, 'sheep': sheep})
+        form = DoseForm(request.POST)
+        if form.is_valid():
+            _save_dose(request, slug)
+            return redirect('sau', slug=slug)
+        else:
+            form = DoseForm(initial={'sheep': current_sheep,
+                                     'date_utc': dt.now(),
+                                     'medicine': request.POST.get('medicine', 1)})
+
+    elif request.method == "GET":
+        form = DoseForm(initial={'sheep': current_sheep, 'date_utc': dt.now()})
+
+    return TemplateResponse(
+        request, 'dose.html', context={
+            'form': form,
+            'sheep': current_sheep
+        })
