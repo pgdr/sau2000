ee1a4df0130b8fd09772288fe8abb5938f2badab
Pål Grønås Drange <PGDR@statoil.com>
2017-12-22 13:52:36 +0100

    yapf forms

--- sau/forms.py
+++ sau/forms.py
@@ -5,11 +5,9 @@ from .models import Sheep, Dose
 class DateInput(forms.DateInput):
     input_type = 'date'
 
-class DoseForm(forms.ModelForm):
 
+class DoseForm(forms.ModelForm):
     class Meta:
         model = Dose
         fields = ('medicine', 'amount', 'date_utc')
-        widgets = {
-            'date_utc': DateInput()
-        }
+        widgets = {'date_utc': DateInput()}
